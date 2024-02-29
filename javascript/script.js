814b87133096bb53a8b076ed8b648c8c1146b774
michealmani <manikandan.mc11@gmail.com>
2024-02-29 07:59:20 -0800

    delete some files

--- javascript/script.js
+++ javascript/script.js
@@ -6,8 +6,4 @@ function javanote2(){
     window.open('varible.html');
     }
     
-var input=document.getElementById("search-in");
-function inputbased(event){
-    var search= event.target.textContent;
-if(search == javaTutorial)
-}
+
