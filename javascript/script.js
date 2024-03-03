2e0912557599b6462122ded41c8675f78689da67
michealmani <manikandan.mc11@gmail.com>
2024-03-03 06:52:15 -0800

    last commit

--- javascript/script.js
+++ javascript/script.js
@@ -1,20 +1,57 @@
-var bt1_java=document.getElementById("bt1-java")
-function javanote(){
-window.open('intro.html');
+var bt1_java = document.getElementById("bt1-java")
+function javanote() {
+    window.location.replace('intro.html');
 }
-function javanote2(){
-    window.open('varible.html');
-    }
-    function javanote3(){
-        window.open('operator.html');
-        }
-        function javanote4(){
-            window.open('control.html');
-            }
-            function javanote5(){
-                window.open('array.html');
-                }
-              
-          
-    
+function javanote2() {
+    window.location.replace('varible.html');
+}
+function javanote3() {
+    window.location.replace('operator.html');
+}
+function javanote4() {
+    window.location.replace('control.html');
+}
+function javanote5() {
+    window.location.replace('array.html');
+}
+function javanote6() {
+    window.location.replace('oops.html');
+}
+function javanote7() {
+    window.location.replace('method.html')
+}
+function javanote8() {
+    window.location.replace('overload.html')
+}
+function javanote9() {
+    window.location.replace('constructor.html')
+}
+function javanote10() {
+    window.location.replace('inherit.html')
+}
+function javanote11() {
+    window.location.replace('override.html')
+}
+function javanote12() {
+    window.location.replace('interface.html')
+}
+function javanote13() {
+    window.location.replace('package.html')
+}
+function javanote14() {
+    window.location.replace('exception.html')
+}
+function javanote15() {
+    window.location.replace('files.html')
+}
+
+function redirectToYoutube() {
+    const url = "youtubevideolink.com"
+    location.replace(url)
+  }
+
+
+
+
+
 
